d91f54dcc2493789cbbacf806cc3c4ad192eaa33
Rajesh Ranganathan <rranga99@gmail.com>
2020-06-14 15:49:33 +0530

    changes to awsrunner

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
+            <h2>Fib Calculator</h2>
             <Link to="/">Home   </Link>
             <Link to="/otherpage">Other Page</Link>
           </div>
